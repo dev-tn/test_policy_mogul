8aea40116ea300fa3703b57ed09cf0c69e521591
Teknuk <dev@teknuk.com>
2019-04-10 13:14:55 +0500

    campaigns - added empty campaigns controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'campaigns/index'
+  get 'campaigns/new'
+
+  root to: 'campaigns#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
