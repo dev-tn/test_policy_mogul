a675cf961505145ab8aa1e06840950a5c05f37e3
Teknuk <dev@teknuk.com>
2019-04-10 17:48:12 +0500

    layout - implemented navbar and sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+$(document).on('turbolinks:load', function () {
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+});
+
